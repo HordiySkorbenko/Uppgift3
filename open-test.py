5d0afad4e61328a94597db1a9ade1428e84bfe12
HordiySkorbenko <hordiy.skrobenko@elev.ga.dbgy.se>
2025-09-12 11:15:41 +0200

    uppgift 4 klar

--- open-test.py
+++ open-test.py
@@ -46,7 +46,18 @@ def add_product(products, name, desc, price, quantity):
         
     return products
 
-def list_products (products):
+def change_product(products, idx, name, price, desc, quantity ):
+    for product in products:
+        if product['id'] == idx:
+                product['name'] = name
+                product['price'] = price
+                product['desc'] = desc
+                product['quantity'] = quantity
+            
+    return products
+            
+
+def list_products(products):
     for idx, product in enumerate(products, 1):
         print(f"{idx}. {product['name']} - {format_currency(product['price'])} - {product['quantity']} st")
 
@@ -58,7 +69,7 @@ def view_product(products, idx):
 
 def product_remove(products, id):
     for product in products:
-        if product['id'] == id:
+        if product['id'] == id - 1:
             products.remove(product)
 
 #TODO: hur gör man så funktionen load_data returnerar products istället?
@@ -78,9 +89,32 @@ product_remove(products, 3)
 add_product(products, "teslacar", "a transport veichle to move around city", 300000, 5)
 
 while True:
-    list_products(products)
-    idx = int(input("välj produkt: "))
+    choice = int(input("Välj vad du vill göra \n1 : hitta ett produkt; 2 : skriv ut alla produkter; 3 : lägga till produkt; 4 : ta bort produkt; 5 : ändra produkt; 6 : se statistik "))
+    if choice == 1 :
+        idx = int(input("välj produkt: "))
+        product = view_product(products,idx)
+        print(f"the product is {product[0]} and costs {product[1]}")
+        
+    elif choice == 2 :    
+        list_products(products)
     
-    product = view_product(products,idx)
-    print(f"the product is {product[0]} and costs {product[1]}")
-    input ("")
+    elif choice == 3 :
+        namn = input("skriv namn på produkten du vill lägga till")
+        desc = input("skriv beskrivning på produkten du vill lägga till")
+        price = float(input("skriv priset på produkten du vill lägga till"))
+        quantity = int(input("skriv mängden av produkten det finns/ska finnas"))
+        add_product(products, namn, desc, price, quantity )
+    
+    elif choice == 4 :
+        idx = int(input("välj produkt att ta bort"))
+        product_remove(products, idx)
+        print(f"du tog bort {products[idx]["name"]} ")
+    
+    elif choice == 5 :
+        idx = int(input("välj produkt att ändra "))
+        namn = input("skriv namn på produkten du vill ändra annars skriv - ")
+        desc = input("skriv beskrivning på produkten du vill ändra annars skriv - ")
+        price = float(input("skriv priset på produkten du vill ändra annars skriv - "))
+        quantity = int(input("skriv mängden av ändrad produkt det finns/ska finnas annars skriv - "))
+        change_product(products, idx, namn, price, desc, quantity )
+        list_products(products)
